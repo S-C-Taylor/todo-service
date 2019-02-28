4592ab17593d4dc7dd2ed3f4297a8748f757d395
Simon Taylor <s.c.taylor@student.unsw.edu.au>
2019-02-28 11:52:02 +1100

    Temporarily enable exception page

--- Startup.cs
+++ Startup.cs
@@ -57,6 +57,7 @@ namespace Todo
             {
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
+                app.UseDeveloperExceptionPage();
             }
 
             app.UseHttpsRedirection();
